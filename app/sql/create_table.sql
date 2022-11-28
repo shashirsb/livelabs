f3b7eaba329a4091baf54fd40220c1353b9fcc59
shashi_bhushan_ramachandra <shashi.bhushan.ramachandra@oracle.com>
2022-11-28 10:36:50 +0530

    adding sql

--- app/sql/create_table.sql
+++ app/sql/create_table.sql
@@ -9,3 +9,7 @@ CREATE TABLE j_onlinecustomerdata
    date_loaded TIMESTAMP (6) WITH TIME ZONE,
    oo_document VARCHAR2 (32767)
    CONSTRAINT ocd_ensure_json CHECK (oo_document IS JSON));
+
+
+create table customerorder(Name varchar2(20),Phone varchar2(20),Location varchar2(20),Card varchar2(20));
+Insert into customerorder values (‘John’,‘3892993328’,‘Jayanagar’,‘3278839833279238’);
--- app/sql/restapi.sql
+++ app/sql/restapi.sql
@@ -1,41 +1,93 @@
--- SPATIAL REST API GET
-
-with x as (SELECT
+-- DEFINE HANDLER
+BEGIN
+    ORDS.DEFINE_HANDLER(
+        p_module_name => 'com.oracle.myfoods',
+        p_pattern => 'frontend',
+        p_method => 'GET',
+        p_source_type => 'source_type_collection_feed',
+        p_source => 'with x as (SELECT
     t1.pk_col,
     t1.pin_code,
     t1.store_address,
     t1.store_name,
     sdo_nn_distance(5)                      distance_m,
-    get_geometry(t1.longitude, t1.latitude) geometry
+    admin.get_geometry(t1.longitude, t1.latitude) geometry
 FROM
     ouser.online_stores t1
 WHERE
         sdo_nn(admin.get_geometry(t1.longitude, t1.latitude),
                mdsys.sdo_geometry(2001,
                                   4326,
-                                  sdo_point_type(1.3222376155648177, 103.84562334102512, NULL),
+                                  sdo_point_type(:long, :lat, NULL),
                                   NULL,
                                   NULL),
-               'sdo_batch_size=10 unit=KM',
-               5) = 'TRUE'
+               ''''sdo_batch_size=10 unit=KM'''',
+               5) = ''''TRUE''''
     AND ROWNUM <= 10
     )
- SELECT 'application/json' as mediatype, -- for ORDS,
-    '{"type": "FeatureCollection", "features":'
+ SELECT ''''application/json'''' as mediatype, -- for ORDS,
+    ''''{"type": "FeatureCollection", "features":''''
     || JSON_ARRAYAGG( 
-        '{"type": "Feature", "properties": {'
-        || '"pk_col":"'|| pk_col
-        ||'","pin_code":"'|| pin_code
-        ||'","store_address":"'|| store_address
-        ||'","store_name":"'|| store_name
-        ||'","distance_m":"'|| distance_m
-        ||'"}, "geometry":'|| SDO_UTIL.TO_GEOJSON(Geometry)
-        ||'}' 
+        ''''{"type": "Feature", "properties": {''''
+        || ''''"pk_col":"''''|| pk_col
+        ||''''","pin_code":"''''|| pin_code
+        ||''''","store_address":"''''|| store_address
+        ||''''","store_name":"''''|| store_name
+        ||''''","distance_m":"''''|| distance_m
+        ||''''"}, "geometry":''''|| SDO_UTIL.TO_GEOJSON(Geometry)
+        ||''''}'''' 
        FORMAT JSON RETURNING CLOB) 
-    ||'}'
+    ||''''}''''
     AS GEOJSON
-FROM X
+FROM X',
+        p_items_per_page=> ,
+        p_comments=> ''
+    );
+    commit;
+END;
 
--- INSERT ORDER REST API POST
+---
 
-INSERT INTO ouser.j_onlineorder VALUES (SYS_GUID(),to_date('25-OCT-2022'),'{"ORDERDATE":"03-06-2022","ORDERTIME":"8:44 PM","ORDER_LINEID":"8","ORDERQUANTITY":"3","ITEMID":"3","ORDERAMOUNT":"830.11","ORDERID":"5","LATITUDE":"12.985","LONGITUDE":"77.5533"}')
+-- DEFINE HANDLER
+BEGIN
+    ORDS.DEFINE_HANDLER(
+        p_module_name => 'com.oracle.myfoods',
+        p_pattern => 'frontend',
+        p_method => 'POST',
+        p_source_type => 'source_type_plsql',
+        p_source => 'INSERT INTO ouser.j_onlineorder VALUES (SYS_GUID(),to_date(''''25-OCT-2022''''),:body)',
+        p_items_per_page=> ,
+        p_comments=> ''
+    );
+    commit;
+END;
+
+
+---
+
+-- DEFINE HANDLER
+BEGIN
+    ORDS.DEFINE_HANDLER(
+        p_module_name => 'com.oracle.myfoods',
+        p_pattern => 'ml',
+        p_method => 'GET',
+        p_source_type => 'source_type_collection_feed',
+        p_source => 'SELECT ROWNUM RANK,
+        CONSEQUENT_NAME RECOMMENDATION,
+        NUMBER_OF_ITEMS NUM,
+        ROUND(RULE_SUPPORT, 3) SUPPORT,
+        ROUND(RULE_CONFIDENCE, 3) CONFIDENCE,
+        ROUND(RULE_LIFT, 3) LIFT,
+        ROUND(RULE_REVCONFIDENCE, 3) REVERSE_CONFIDENCE
+        FROM (SELECT * FROM ouser.DM$VRGB_DISH_MB
+        WHERE NUMBER_OF_ITEMS = 2
+        AND EXTRACT(antecedent, ''''//item[item_name="''''||to_char(:item)||''''"]'''') IS NOT NULL
+        ORDER BY NUMBER_OF_ITEMS
+        )
+        WHERE ROWNUM <= 2',
+        p_items_per_page=> 25,
+        p_comments=> ''
+    );
+    commit;
+END;
+
