c8999b0a94d5be9186d9c99876a8fca947e583ee
shashi_bhushan_ramachandra <shashi.bhushan.ramachandra@oracle.com>
2022-11-28 14:03:20 +0530

    commit the sql script

--- app/sql/create_table.sql
+++ app/sql/create_table.sql
@@ -1,15 +1,14 @@
-CREATE TABLE j_onlineorder
+CREATE TABLE ouser.j_onlineorder
   (id          VARCHAR2 (32) NOT NULL PRIMARY KEY,
    date_loaded TIMESTAMP (6) WITH TIME ZONE,
    oo_document VARCHAR2 (32767)
    CONSTRAINT oo_ensure_json CHECK (oo_document IS JSON));
 
-CREATE TABLE j_onlinecustomerdata
+CREATE TABLE ouser.j_onlinecustomerdata
   (id          VARCHAR2 (32) NOT NULL PRIMARY KEY,
    date_loaded TIMESTAMP (6) WITH TIME ZONE,
    oo_document VARCHAR2 (32767)
    CONSTRAINT ocd_ensure_json CHECK (oo_document IS JSON));
 
 
-create table customerorder(Name varchar2(20),Phone varchar2(20),Location varchar2(20),Card varchar2(20));
-Insert into customerorder values (‘John’,‘3892993328’,‘Jayanagar’,‘3278839833279238’);
+create table ouser.customerorder(Name varchar2(20),Phone varchar2(20),Location varchar2(20),Card varchar2(20));
